d1888887e06f989540a129e3bebd5169b387834b
Jerem <jeremie.primas@gmail.com>
2023-01-21 00:42:53 +0100

    Add test project

---
+++ test/src/api/test.api.ts
@@ -0,0 +1,35 @@
+import { Controller, controller, httpGet, httpPost, httpPut, httpDelete, fromBody } from '../../../src/lib/api-interface';
+
+@controller('api/Test')
+export class TestApi implements Controller
+{
+    @httpGet('foo/:param1/bar/:param2')
+    public get1(param1: number, param2: string, param3: string): any
+    {
+        return { param1, param2, param3 };
+    }
+
+    @httpGet('id/:id')
+    public get2(id: number, language: string): any
+    {
+        return { id, language }
+    }
+
+    @httpPost(':param1')
+    public post1(param1: number, param2: string, @fromBody body: any): any
+    {
+        return { param1, param2, body };
+    }
+
+    @httpPut(':param1')
+    public put1(param1: number, @fromBody body: any): any
+    {
+        return { param1, body };
+    }
+
+    @httpDelete('foo/bar/:id')
+    public delete1(id: number): any
+    {
+        return { id };
+    }
+}
---
+++ test/src/test-index.ts
@@ -0,0 +1,85 @@
+import { ApiManager } from '../../src/lib/api-manager';
+import { TestApi } from './api/test.api';
+import { WebClient } from '@jeje-devs/web-client';
+import { Server } from 'http';
+
+const port = 3071;
+const baseApiTestRoute = `http://localhost:${port}/api/Test`;
+
+ApiManager.run(port, async server => await runTests(server), _ => { }, [new TestApi()]);
+
+async function runTests(server: Server): Promise<void>
+{
+    for (const method of testMethods)
+    {
+        await method();
+    }
+    console.log("All tests ran successfully!");
+    server.close();
+}
+
+function assert(predicate: () => boolean): void
+{
+    if (!predicate())
+    {
+        throw new Error('Test failed');
+    }
+}
+
+const testMethods: Array<() => Promise<void>> = [
+
+    async () =>
+    {
+        const expected = { param1: 14, param2: 'hello', param3: 'baz' };
+        const actual = await WebClient.get<any>(baseApiTestRoute + '/foo/14/bar/hello?param3=baz');
+
+        assert(() => Object.keys(actual).length === Object.keys(expected).length);
+        assert(() => actual.param1 === expected.param1);
+        assert(() => actual.param2 === expected.param2);
+        assert(() => actual.param3 === expected.param3);
+    },
+
+    async () =>
+    {
+        const expected = { id: 7840, language: 'fr-CH' };
+        const actual = await WebClient.get<any>(baseApiTestRoute + '/id/7840?language=fr-CH');
+
+        assert(() => Object.keys(actual).length === Object.keys(expected).length);
+        assert(() => actual.id === expected.id);
+        assert(() => actual.language === expected.language);
+    },
+
+    async () =>
+    {
+        const expected = { param1: 45, param2: 'Hola', body: { id: 12, name: 'FooBar' } };
+        const actual = await WebClient.post<any>(baseApiTestRoute + '/45?param2=Hola', { id: 12, name: 'FooBar' });
+
+        assert(() => Object.keys(actual).length === Object.keys(expected).length);
+        assert(() => actual.param1 === expected.param1);
+        assert(() => actual.param2 === expected.param2);
+        assert(() => Object.keys(actual.body).length === Object.keys(expected.body).length);
+        assert(() => actual.body.id === expected.body.id);
+        assert(() => actual.body.name === expected.body.name);
+    },
+
+    async () =>
+    {
+        const expected = { param1: 2, body: { boo: true, val: 'Here' } };
+        const actual = await WebClient.put<any>(baseApiTestRoute + '/2', { boo: true, val: 'Here' });
+
+        assert(() => Object.keys(actual).length === Object.keys(expected).length);
+        assert(() => actual.param1 === expected.param1);
+        assert(() => Object.keys(actual.body).length === Object.keys(expected.body).length);
+        assert(() => actual.body.boo === expected.body.boo);
+        assert(() => actual.body.val === expected.body.val);
+    },
+
+    async () =>
+    {
+        const expected = { id: 7145 };
+        const actual = await WebClient.delete<any>(baseApiTestRoute + '/foo/bar/7145');
+
+        assert(() => Object.keys(actual).length === Object.keys(expected).length);
+        assert(() => actual.id === expected.id);
+    }
+];
